031ed2e890150f1ee9d1d83548787884dcce0bdc
Prashant Rai <prashantrai230@gmail.com>
2022-03-28 16:46:18 +0530

    All files updated

--- password.py
+++ password.py
@@ -1,18 +1,17 @@
-print("Welcome to password checker! \n")
 from playsound import playsound
 import os
 import time
+wc=0 
 print(os.path.dirname(__file__))
-wc=0
 while 1:
     print('\nEnter Password : ',end='')
     s=input()
     if s!='password':
         wc+=1
-        print('\t\tWrong Password. You may not enter.')
+        print('\t\tWrong Password. You may not enter.',wc)
         playsound(os.path.dirname(__file__)+'\error.mp3')
         if wc==5:
-            print('\n\n\tToo many wrong attempts...Try again after 10 seconds\n')
+            print('\n\n\tToo many wrong attempts...Try after 30 seconds\n')
             t=10
             while t!=-1:
                 print('Retry in {:02d} seconds...'.format(t),end='\r')
@@ -25,8 +24,3 @@ while 1:
         print('\n\t\tWelcome. You may enter.\n')
         playsound(os.path.dirname(__file__)+'\correct.mp3')
         exit()
-
-# This is a password checker application.
-# correct password is "password."
-# Maximum 5 attempts allowed
-# Sound alerts for correct and incorrect password
